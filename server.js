23fd000800cd5b1f02bf278f2793956034fd75c6
Rakesh Yarlagadda <ry2294@columbia.edu>
2016-05-09 14:27:21 +0000

    Final Push

--- server.js
+++ server.js
@@ -9,25 +9,23 @@ var _ = require('underscore');
 app.use(bodyParser.json());
 
 AWS.config.update({
-    accessKeyId: 'AKIAJCAQWMNEIC7J7AWA', 
-    secretAccessKey: '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',
+    accessKeyId: '', 
+    secretAccessKey: '',
     region: 'us-west-2'});
     
 var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
-var dynamodb = new AWS.DynamoDB();
 
 router.get('/', function(req, res) {
   res.sendfile(path.join(__dirname + '/home.html'));
 });
 
-router.post('/tweet', function(req, res) {
+router.post('/comment', function(req, res) {
+    console.log('/comment req = ' + JSON.stringify(req.body));
     dynamodbDoc.put({
         TableName: req.body.table,
         Item: {
-            "tweetid": String(req.body.tweetid),
-            "text": req.body.text,
-            "lat": req.body.lat,
-            "lon": req.body.lon
+            "id": String(req.body.id),
+            "text": req.body.text
         }
     }, function(error, data) {
         if(error) console.log("error = " + JSON.stringify(error));
@@ -41,71 +39,32 @@ var http = require('http').Server(app);
 var server = http.listen(process.env.PORT || 5000, function(){
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('App listening at http://%s:%s', host, port);
 });
 
 var io = require('socket.io').listen(server);
 io.on('connection', function(socket){
   console.log('a user connected');
-  dynamodb.scan({TableName: "javatweet"}, function(error, tweets) {
-	if (error) console.log(JSON.stringify(error));
-	else {
-		console.log(JSON.stringify(tweets.Items[0]));
-		_.each(tweets.Items, function(rawtweet) {
-		    if(rawtweet.lat != null && rawtweet.lon != null && rawtweet.sentiment != null &&
-		    rawtweet.text != null && rawtweet.time != null) {
-		        var tweet = {};tweet.lat = rawtweet.lat.S;tweet.lon = rawtweet.lon.S;
-    		    tweet.sentiment = rawtweet.sentiment.S;tweet.text = rawtweet.text.S;
-    		    tweet.time = rawtweet.time.S;
-    		    io.emit("javatweet", tweet);
-		    }
-		});
-	}
-  });
-  
-  dynamodb.scan({TableName: "cplusplustweet"}, function(error, tweets) {
-	if (error) console.log(JSON.stringify(error));
-	else {
-		console.log(JSON.stringify(tweets.Items[0]));
-		_.each(tweets.Items, function(rawtweet) {
-		    if(rawtweet.lat != null && rawtweet.lon != null && rawtweet.sentiment != null &&
-		    rawtweet.text != null && rawtweet.time != null) {
-		        var tweet = {};tweet.lat = rawtweet.lat.S;tweet.lon = rawtweet.lon.S;
-    		    tweet.sentiment = rawtweet.sentiment.S;tweet.text = rawtweet.text.S;
-    		    tweet.time = rawtweet.time.S;
-    		    io.emit("cplusplustweet", tweet);
-		    }
-		});
-	}
-  });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
 
 router.post('/time', function(req, res) {
+    console.log('/time req = ' + JSON.stringify(req.body));
     dynamodbDoc.put({
         TableName: req.body.table,
         Item: {
-            "tweetid": String(req.body.tweetid),
+            "id": String(req.body.id),
             "text": req.body.text,
-            "lat": req.body.lat,
-            "lon": req.body.lon,
             "sentiment": req.body.sentiment,
             "time": req.body.time
         }
     }, function(error, data) {
         if(error) console.log("error = " + JSON.stringify(error));
         res.status(200).end();
-        var tweet = {};
-        tweet.lat = req.body.lat;tweet.lon = req.body.lon;tweet.time = req.body.time;
-    	tweet.sentiment = req.body.sentiment;tweet.text = req.body.text;
-        if(req.body.table == "javatweet") {
-            io.emit('javatweet', tweet);
-        } else {
-            io.emit('cplusplustweet', tweet);
-        }
+        var comment = {}; comment.time = req.body.time;
+    	comment.sentiment = req.body.sentiment;comment.text = req.body.text;
+        io.emit(req.body.table, comment);
     });
 });
-
